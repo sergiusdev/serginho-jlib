8aa77136bcfef768a4d23232d02cf23db05b6d01
Antônio Sérgio Garcia Ferreira <antoniosergius@gmail.com>
2017-03-13 11:40:40 -0300

    Melhora o nome de alguns métodos

--- src/br/com/antoniosergius/lib/tools/Convert.java
+++ src/br/com/antoniosergius/lib/tools/Convert.java
@@ -9,53 +9,55 @@ public class Convert {
         return str == null ? "" : str.trim();
     }
     
-    public static GregorianCalendar toGregorian(String strData, String formato) {
-        if (!Parse.date(strData, formato)) {
+    public static GregorianCalendar strToGregorian(String strDate, String format) {
+        if (!Parse.date(strDate, format)) {
             return null;
         }
-        String []dateFields = strData.split("/");
-        int ano;
-        if (formato.equals("dd/MM/yy")) {
-            ano = 2000 + Integer.parseInt(dateFields[2]);
+        String []dateFields = strDate.split("/");
+        int year;
+        if (format.equals("dd/MM/yy")) {
+            year = 2000 + Integer.parseInt(dateFields[2]);
         } else {
-            ano = Integer.parseInt(dateFields[2]);
+            year = Integer.parseInt(dateFields[2]);
         }
         
-        GregorianCalendar objData = new GregorianCalendar(ano, 
+        GregorianCalendar gregorianObj = new GregorianCalendar(year, 
                                                           Integer.parseInt(dateFields[1])-1,
                                                           Integer.parseInt(dateFields[0]));
-        return objData;
+        return gregorianObj;
     }
     
-    public static Date toDate(GregorianCalendar gregorianCal) {
+    public static Date gregorianToDate(GregorianCalendar gregorianCal) {
         Date date = new Date(gregorianCal.getTimeInMillis());
         return date;
     }
     
-    public static GregorianCalendar toGregorian(String strDate) {
+    public static GregorianCalendar strToGregorian(String strDate) {
         if (!Parse.date(strDate)) {
             return null;
         }
         String []dateField = strDate.split("/");
-        GregorianCalendar date = new GregorianCalendar(Integer.parseInt(dateField[2]), 
+        GregorianCalendar gregorianObj = new GregorianCalendar(Integer.parseInt(dateField[2]), 
                                                           Integer.parseInt(dateField[1])-1,
                                                           Integer.parseInt(dateField[0]));
-        return date;
+        return gregorianObj;
     }
     
-    public static GregorianCalendar toGregorianTimestamp(String strDate) {
-        if (!Parse.timestamp(strDate)) {
+    public static GregorianCalendar strTimestampToGregorian(String strTimestamp) {
+        if (!Parse.timestamp(strTimestamp)) {
             return null;
         }
-        String []dateAndHour = strDate.split(" ");
+        String []dateAndHour = strTimestamp.split(" ");
         String []dataField = dateAndHour[0].split("/");
-        GregorianCalendar date = new GregorianCalendar(Integer.parseInt(dataField[2]), 
+        GregorianCalendar gregorianObj = new GregorianCalendar(Integer.parseInt(dataField[2]), 
                                                        Integer.parseInt(dataField[1])-1,
                                                        Integer.parseInt(dataField[0]));
         String []hourField = dateAndHour[1].split(":");
-        date.set(GregorianCalendar.HOUR_OF_DAY, Integer.parseInt(hourField[0]));
-        date.set(GregorianCalendar.MINUTE, Integer.parseInt(hourField[1]));
-        return date;
+        gregorianObj.set(GregorianCalendar.HOUR_OF_DAY, Integer.parseInt(hourField[0]));
+        gregorianObj.set(GregorianCalendar.MINUTE, Integer.parseInt(hourField[1]));
+        return gregorianObj;
     }
+    
+    
     
 }
